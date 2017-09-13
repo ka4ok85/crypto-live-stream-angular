1b8f2d28232da342fbad07bc590a29f5f594969e
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2017-09-13 16:31:04 -0400

    clean up

--- src/app/rate.component.ts
+++ src/app/rate.component.ts
@@ -20,20 +20,14 @@ export class RateComponent {
     private apiURL = 'http://localhost:8080/stream?currency=BTC';
 
     public rawData = [];
-    public barChartData: Array<any> = [{ data: [] }];
-    public barChartLabels: string[] = [];
-
-
 
     public lineChartData: Array<any> = [{ data: [] }];
     public lineChartLabels: Array<any> = [];
-
 
     constructor(private http: Http, private route: ActivatedRoute, private titleService: Title) {
         this.currency = route.snapshot.paramMap.get('currency');
 
         console.log(this.currency);
-
     }
 
     ngOnInit() {
@@ -46,89 +40,30 @@ export class RateComponent {
         this.getData(this.currency, this.apiURL);
     }
 
-    public test(e) {
-        
+    public processMessage(e) {
         let data = JSON.parse(e.data);
-
-        let dataLabels: string[] = [];
-        let dataCounts: string[] = [];
         this.updateLineChart(data.lastUpdate, data.price);
-        //this.buildBarChart(dataLabels, dataCounts);            
-
-
-
         //this.busy = this.theDataSource.toPromise();
     }
 
 
     public getData(currency: string, url: string) {
-
-        console.log("getData");
-
         var source = new EventSource(url);
-        //source.addEventListener('message', function(e) {
-        //source.addEventListener("message", this.test, false);
-        source.addEventListener("message", this.test.bind(this), false);
-
-// this.handleDataLoaded.bind(this)
-
-        /*
-            let data = JSON.parse(e.data);
-            console.log(data);
-
-//test();
-
-            let dataLabels: string[] = [];
-            let dataCounts: string[] = [];
-
-
-            let p : string;
-            let l : string;
-
-            l = ""+data.lastUpdate;
-            p = ""+data.price;
-
-            dataLabels.push(l);
-            dataCounts.push(p);
-
-            console.log( "11111");
-            console.log( data.lastUpdate);
-            console.log( data.price); 
-
-            console.log( "222");
-            console.log( dataCounts);
-            console.log( dataLabels);
-
-
-            this.buildBarChart(dataLabels, dataCounts);            
-          }, false);
-
-
-        //this.busy = this.theDataSource.toPromise();
-
-  */      this.rawData = [];
-
-
+        source.addEventListener("message", this.processMessage.bind(this), false);
     }
 
     /* CHARTS */
-    public updateLineChart(label: number, dataCount: number) {
+    public updateLineChart(timestamp: number, price: number) {
         console.log("updateLineChart running");
 
-        //console.log(this.lineChartData);
-        //console.log(this.lineChartData[0]);
-        let x: number[];
-        x = this.lineChartData[0].data;
-        //console.log(x);
-        x.push(dataCount);
-        console.log(x);
+        let rates: number[];
+        rates = this.lineChartData[0].data;
 
-        
-        let date = new Date(label*1000);
+        rates.push(price);
+       
+        let date = new Date(timestamp*1000);
 
-        //new Date()
-        //console.log(label); 
-        console.log(date);
+        console.log("Date: " + date);
         let year = date.getFullYear();
         let month = this.pad(date.getMonth() + 1, 2);
         let day = this.pad(date.getDate(), 2);
@@ -139,11 +74,11 @@ export class RateComponent {
         let l = year + '-' + month + '-' + day + ' ' + hour + ':' + minute + ':' + second;
         console.log(l);
 
-        this.lineChartData = [{ data: x, label: 'Rate' }];
+        this.lineChartData = [{ data: rates, label: 'Live Rate' }];
         this.lineChartLabels.push(l);
 
     }
-
+/*
     private buildLineChart(labels: String[], dataCounts: String[]) {
         this.lineChartData = [{ data: dataCounts, label: 'Number of 9-1-1 calls per day' }];
         let labelsCount = this.lineChartLabels.length;
@@ -155,7 +90,7 @@ export class RateComponent {
             this.lineChartLabels.push(label);
         }
     }
-
+*/
     public lineChartOptions: any = {
         responsive: true,
         scales: {
@@ -178,10 +113,6 @@ export class RateComponent {
     };
 
     public lineChartType: string = 'line';
-    public chartClicked(e: any): void {
-    }
-    public chartHovered(e: any): void {
-    }
     public lineChartColors: Array<any> = [
         {
             backgroundColor: 'rgba(0, 131, 154,0.2)',
@@ -193,7 +124,6 @@ export class RateComponent {
         }
     ];
     public lineChartLegend: boolean = true;
-
 
     public pad(num: number, size: number): string {
         var s = num + "";
