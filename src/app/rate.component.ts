6285ab7c9c954ec407d0b22cb0a766099e0181cc
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2017-09-18 20:21:48 -0400

    added http call for retrieving history data

--- src/app/rate.component.ts
+++ src/app/rate.component.ts
@@ -5,6 +5,7 @@ import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Observable } from "rxjs/Observable";
 import EventSource from 'eventsource';
+import { environment } from '../environments/environment';
 
 
 @Component({
@@ -17,7 +18,13 @@ import EventSource from 'eventsource';
 export class RateComponent {
     
     public currency: string;
-    private apiURL = 'http://localhost:8080/stream?currency=BTC';
+    theDataSource: Observable<string>;
+
+    private apiUrl = environment.apiUrl;
+    private apiStreamUrlString = "stream?currency=";
+    private apiHistoryUrlString = "last?currency=";
+    private apiStreamUrl = "";
+    private apiHistoryUrl = "";
 
     public rawData = [];
 
@@ -26,18 +33,27 @@ export class RateComponent {
 
     constructor(private http: Http, private route: ActivatedRoute, private titleService: Title) {
         this.currency = route.snapshot.paramMap.get('currency');
-
+        console.log(this.apiUrl);
         console.log(this.currency);
     }
 
     ngOnInit() {
+        this.route.data
+        .subscribe((data: { }) => {
+            this.titleService.setTitle('Rates for ' + this.currency + ' / USD');
+            this.apiHistoryUrl = this.apiUrl + this.apiHistoryUrlString + this.currency;
+        });
+
+        this.getHistoryData(this.currency, this.apiHistoryUrl);
+        /*
         this.route.data
             .subscribe((data: { }) => {
                 this.titleService.setTitle('Rates for ' + this.currency + ' / USD');
                 //this.apiURL = data.envSpecific.apiURL;
             });
 
-        this.getData(this.currency, this.apiURL);
+
+*/
     }
 
     public processMessage(e) {
@@ -46,6 +62,50 @@ export class RateComponent {
         //this.busy = this.theDataSource.toPromise();
     }
 
+    public getHistoryData(currency: string, url: string) {
+        console.log(url);
+        this.theDataSource = this.http.get(url).map(res => res.json());
+        this.rawData = [];
+        
+                // Get the data from the REST server
+                this.theDataSource.subscribe(
+                    data => {
+console.log(data);
+                        let dataLabels: string[] = [];
+                        let dataCounts: string[] = [];
+        
+                        for (let i = 0; i < data.length; i++) {
+                            this.rawData.push([data[i][0], data[i][1]]);
+                        }
+        
+                        this.rawData.sort(function (count1, count2) {
+                            if (count1[0] < count2[0]) {
+                                return 1;
+                            } else if (count1[0] > count2[0]) {
+                                return -1;
+                            } else {
+                                return 0;
+                            }
+                        });
+        
+                        let limit: number;
+                        if (this.rawData.length > 5) {
+                            limit = 5;
+                        } else {
+                            limit = this.rawData.length;
+                        }
+        
+                        for (let i = 0; i < limit; i++) {
+                            dataLabels.push(this.rawData[i][1]);
+                            dataCounts.push(this.rawData[i][0]);
+                        }
+        
+                        //this.buildBarChart(dataLabels, dataCounts);
+                    },
+                    err => console.log("Can't get Counts. Error code: %s, URL: %s ", err.status, err.url),
+                    () => console.log('Counts are retrieved')
+                );
+    }
 
     public getData(currency: string, url: string) {
         var source = new EventSource(url);
@@ -124,6 +184,10 @@ export class RateComponent {
         }
     ];
     public lineChartLegend: boolean = true;
+    public chartClicked(e: any): void {
+    }
+    public chartHovered(e: any): void {
+    }
 
     public pad(num: number, size: number): string {
         var s = num + "";
