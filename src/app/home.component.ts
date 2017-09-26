e55ab8ff827269332ac5018e8ac3b7983b6f11bf
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2017-09-25 20:24:20 -0400

    added title to home component

--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
     selector: '',
@@ -8,7 +10,15 @@ import { Component, OnInit } from '@angular/core';
 
 export class HomeComponent {
 
+    constructor(private route: ActivatedRoute, private titleService: Title) {
+
+    }
+
     ngOnInit() {
+        this.route.data
+            .subscribe((data: {}) => {
+                this.titleService.setTitle('Live Crypto Rates');
+            });
     }
 
 }
